47101f2e97d82474ccd6d92f2bc81d93302f07c8
LucasE2996 <moeda.60@gmail.com>
2019-04-16 00:40:19 -0300

    update

--- vagrant/restaurant-menu-CRUD/db/restaurants_repository.py
+++ vagrant/restaurant-menu-CRUD/db/restaurants_repository.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
-from pprint import pprint
 
 engine = create_engine('sqlite:///restaurantMenu.db')
 Base.metadata.bind = engine
